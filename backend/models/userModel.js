42f2b7752b6f745353358d69a7a3ecde6ce9eff2
zarick1 <krstozaric01@gmail.com>
2025-06-06 13:28:19 +0200

    Bcrypt password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   username: {
@@ -15,7 +16,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
-      // Workd only on save !!!
+      // Workd only on create and save !!!
       validator: function (val) {
         return this.password === val;
       },
@@ -30,6 +31,16 @@ const userSchema = mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Pre-save middleware to hash the password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
