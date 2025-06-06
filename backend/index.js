bec5d3618c963a4fcbecc65f5fc8e5da94b93f00
zarick1 <krstozaric01@gmail.com>
2025-06-06 12:22:12 +0200

    Connect base, run server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config({ path: './config.env' });
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('Data Base Successfuly connected'));
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App running on ${port}...`);
+});
